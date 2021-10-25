d1406b5c01d63485da5657ae0209680665b57c62
Juan Pablo Varela <33493616+jvarelaaloisio@users.noreply.github.com>
2021-10-25 16:33:08 -0300

    Added ruins to the whitebox

---
+++ U_Drimys/Assets/Scripts/Extras/CameraGameDesignHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraGameDesignHelper : MonoBehaviour
+{
+	[SerializeField]
+	private bool controlPosition;
+
+	[SerializeField]
+	private float yOffset;
+
+	
+	
+	private void Update()
+	{
+		var transformPosition = transform.position;
+		if (controlPosition
+			&& transform.hasChanged
+			&& Physics.Raycast(transformPosition,
+													Vector3.down,
+													out RaycastHit hit,
+													10))
+		{
+			transform.position = new Vector3(transformPosition.x,
+											hit.point.y + yOffset,
+											transformPosition.z);
+		}
+	}
+}
