41a8b1c2d0498da3683310af8233e4fed94fcc68
Juan Pablo Varela <33493616+jvarelaaloisio@users.noreply.github.com>
2021-11-04 17:56:02 -0300

    Added the character fbx and a sceneloader for testing

---
+++ U_Drimys/Assets/Scripts/Extras/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	[SerializeField]
+	private int[] sceneIds;
+
+	private Scene[] _scenes;
+
+	private void Start()
+	{
+		for (int id = 0; id < sceneIds.Length; id++)
+		{
+			SceneManager.LoadSceneAsync(sceneIds[id], LoadSceneMode.Additive);
+		}
+	}
+}
