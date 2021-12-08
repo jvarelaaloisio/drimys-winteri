e1719c60921a5910dfcf88a792abce9720dabc56
Juan Pablo Varela <33493616+jvarelaaloisio@users.noreply.github.com>
2021-12-08 18:54:06 -0300

    Added House to level 1

---
+++ U_Drimys/Assets/Scripts/Events/StringChannelPropagator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Events.Channels;
+using Events.UnityEvents;
+using UnityEngine;
+
+namespace Events
+{
+	public class StringChannelPropagator : MonoBehaviour 
+	{
+		[SerializeField, Tooltip("Not Null")]
+		private StringChannelSo channel;
+		
+		public StringUnityEvent onEvent;
+
+		private void Awake()
+		{
+			try
+			{
+				channel.Subscribe(onEvent.Invoke);
+			}
+			catch (NullReferenceException)
+			{
+				throw new NullReferenceException($"No channel was provided for the propagation" +
+												$" in the {gameObject.name} GameObject");
+			}
+		}
+	}
+}
